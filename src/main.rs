bff23ec58a31f6ec7fec60d8b2c910123e2e99cd
Mae Dartmann <hello@maedartmann.name>
2023-02-24 20:38:01 +0100

    more cleanup!
    better code, less for_each, more safety, more serialization
    
     Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use serde::Deserialize;
 use serde_json::Value;
-use std::vec;
+use std::{thread::sleep, time::Duration, vec};
 use thiserror::Error;
 
-const STATION_IDS: [i32; 18] = [
+const STATION_IDS: &[i32] = &[
     252,  // Rathaus – 2 (Richtung Friedrich-Engels-Platz)
     269,  // Rathaus – 2 (Richtung Dornbach)
     4205, // Rathaus – U2 (gesperrt)
@@ -23,6 +24,8 @@ const STATION_IDS: [i32; 18] = [
     5691, // Auerspergstraße – N46 (stadtauswärts)
 ];
 
+const API_URL: &str = "http://www.wienerlinien.at/ogd_realtime/monitor/";
+
 #[derive(Error, Debug)]
 enum ApiRequestError {
     #[error("API request failed: {0}")]
@@ -36,16 +39,15 @@ enum ApiRequestError {
 }
 
 struct WienerLinienAPIRequest {
-    url: String,
     traffic_info: String,
     stop_id: Vec<i32>,
 }
 
 impl WienerLinienAPIRequest {
     fn to_req_url(&self) -> String {
-       format!(
+        format!(
             "{}?activateTrafficInfo={}{}",
-            self.url,
+            API_URL,
             self.traffic_info,
             self.stop_id
                 .iter()
@@ -62,115 +64,58 @@ struct WienerLinienMonitor {
     lines: Vec<WienerLinienLine>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 struct WienerLinienLocationStop {
-    _coordinates: [f64; 2],
+    #[serde(rename = "geometry")]
+    geometry: StopGeometry,
+    #[serde(rename = "properties")]
+    properties: StopProperties,
+}
+#[derive(Debug, Clone, Deserialize)]
+struct StopGeometry {
+    coordinates: [f32; 2],
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct StopProperties {
     title: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
 struct WienerLinienLine {
     name: String,
+    #[serde(rename = "towards")]
     destination: String,
+    #[serde(rename = "type")]
     vehicle_type: String,
-    departures: Vec<WienerLinienLineDeparture>,
+    departures: WienerLinienLineDepartures,
 }
 
-#[allow(non_snake_case)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize)]
+struct WienerLinienLineDepartures {
+    departure: Vec<WienerLinienLineDeparture>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 struct WienerLinienLineDeparture {
-    timePlanned: iso8601::DateTime,
-    timeReal: iso8601::DateTime,
-    timeRealIsPresent: bool,
+    #[serde(rename = "departureTime")]
+    departure_time: WienerLinienLineDepartureTime,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct WienerLinienLineDepartureTime {
+    #[serde(rename = "timePlanned")]
+    time_planned: String,
+    #[serde(rename = "timeReal")]
+    time_real: Option<String>,
     countdown: i64,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Deserialize)]
 struct WienerLinienTrafficInfo {
     priority: String,
     title: String,
     description: String,
-}
-
-impl WienerLinienTrafficInfo {
-    /// - Assemble WienerLinienTrafficInfo object from JSON API output
-    /// - Expects an element from the "trafficInfos" array
-    fn assemble_from_json(input: &serde_json::Value) -> Self {
-        Self {
-            priority: input["priority"].as_str().unwrap().to_string(),
-            description: input["description"].as_str().unwrap().to_string(),
-            title: input["title"].as_str().unwrap().to_string(),
-        }
-    }
-}
-
-impl WienerLinienLocationStop {
-    /// - Assemble WienerLinienLocationStop object from JSON API output
-    /// - Expects an index from the monitors array
-    fn assemble_from_json(input: &serde_json::Value) -> Self {
-        let location_stop_value = input["locationStop"].clone();
-        let coords_value = location_stop_value["geometry"]["coordinates"]
-            .as_array()
-            .unwrap();
-        Self {
-            _coordinates: [
-                coords_value[0].as_f64().unwrap(),
-                coords_value[1].as_f64().unwrap(),
-            ],
-            title: location_stop_value["properties"]["title"]
-                .as_str()
-                .unwrap()
-                .to_string(),
-        }
-    }
-}
-
-impl WienerLinienLineDeparture {
-    /// - Assemble WienerLinienLineDeparture object from JSON API output
-    /// - Expects Index from `lines.departures.departure` array
-    fn assemble_from_json(input: &serde_json::Value) -> Self {
-        let departure_value = input["departureTime"].clone();
-        let departure_time_real: iso8601::DateTime;
-        let departure_time_real_present: bool;
-        if departure_value["timeReal"].is_string() {
-            departure_time_real =
-                iso8601::datetime(departure_value["timeReal"].as_str().unwrap()).unwrap();
-            departure_time_real_present = true;
-        } else {
-            departure_time_real = iso8601::datetime("2000-01-01T00:00").unwrap();
-            departure_time_real_present = false;
-        }
-        Self {
-            timePlanned: iso8601::datetime(departure_value["timePlanned"].as_str().unwrap())
-                .unwrap(),
-            timeReal: departure_time_real,
-            timeRealIsPresent: departure_time_real_present,
-            countdown: departure_value["countdown"].as_i64().unwrap(),
-        }
-    }
-}
-
-impl WienerLinienLine {
-    /// - Assemble WienerLinienLine object from JSON API output
-    /// - Expects element from `monitors[].lines` array
-    fn assemble_from_json(input: &serde_json::Value) -> Self {
-        let t_name = input["name"].as_str().unwrap();
-        let t_vtype = input["type"].as_str().unwrap();
-        let t_destination = input["towards"].as_str().unwrap();
-        let departure_raw_array = input["departures"]["departure"].as_array().unwrap();
-        let mut departure_parsed_array: Vec<WienerLinienLineDeparture> = vec![];
-        departure_raw_array.iter().for_each(|x| {
-            departure_parsed_array.push(WienerLinienLineDeparture::assemble_from_json(x));
-        });
-
-        departure_parsed_array.sort_by(|a, b| a.countdown.cmp(&b.countdown));
-        Self {
-            name: t_name.to_string(),
-            vehicle_type: t_vtype.to_string(),
-            departures: departure_parsed_array,
-            destination: t_destination.to_string(),
-        }
-    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -189,7 +134,8 @@ struct Line {
 
 #[derive(Clone, Eq)]
 struct Departure {
-    time: iso8601::DateTime,
+    time_planned: String,
+    time_real: Option<String>,
     countdown: i64,
     station_name: String,
     destination_name: String,
@@ -213,17 +159,19 @@ impl Line {
 
 impl Departure {
     fn from_wiener_linien_api(
-        t_line: WienerLinienLine,
-        t_time: iso8601::DateTime,
-        t_countdown: i64,
-        t_station_name: String,
+        t_line: &WienerLinienLine,
+        t_time_planned: &str,
+        t_time_real: &Option<String>,
+        t_countdown: &i64,
+        t_station_name: &str,
     ) -> Self {
         Departure {
-            line: Line::from_wiener_linien_line(&t_line),
-            time: t_time,
-            countdown: t_countdown,
-            destination_name: t_line.destination,
-            station_name: t_station_name,
+            line: Line::from_wiener_linien_line(t_line),
+            time_planned: t_time_planned.to_owned(),
+            time_real: t_time_real.clone(),
+            countdown: *t_countdown,
+            destination_name: t_line.destination.clone(),
+            station_name: t_station_name.to_owned(),
         }
     }
 }
@@ -245,7 +193,7 @@ impl PartialEq for Departure {
         self.line == other.line
             && self.destination_name == other.destination_name
             && self.station_name == other.station_name
-            && self.time == other.time
+            && self.time_planned == other.time_planned
     }
 }
 
@@ -258,30 +206,22 @@ impl PartialEq for Line {
 async fn get_data_from_api(req: &WienerLinienAPIRequest) -> Result<String, reqwest::Error> {
     let res = reqwest::get(req.to_req_url()).await;
 
-    if res.is_err() {
-        return Err(res.err().unwrap());
-    }
-
     return res?.text().await;
 }
 
 async fn make_api_request(
 ) -> Result<(Vec<Departure>, Option<Vec<WienerLinienTrafficInfo>>), ApiRequestError> {
     let reqobj = WienerLinienAPIRequest {
-        url: "http://www.wienerlinien.at/ogd_realtime/monitor/".to_string(),
         traffic_info: "stoerunglang".to_string(),
         stop_id: STATION_IDS.to_vec(),
     };
 
-    let response_text = match get_data_from_api(&reqobj).await {
-        Ok(text) => text,
-        Err(e) => return Err(ApiRequestError::ApiReqFailed(e)),
-    };
+    let response_text = get_data_from_api(&reqobj)
+        .await
+        .map_err(ApiRequestError::ApiReqFailed)?;
 
-    let response_json: Value = match serde_json::from_str(&response_text) {
-        Ok(json) => json,
-        Err(e) => return Err(ApiRequestError::JsonParsingFailed(e)),
-    };
+    let response_json: Value =
+        serde_json::from_str(&response_text).map_err(ApiRequestError::JsonParsingFailed)?;
 
     let response_trafficinfo_json = response_json["data"]["trafficInfos"].as_array();
 
@@ -291,12 +231,17 @@ async fn make_api_request(
 
     let mut wl_monitors: Vec<WienerLinienMonitor> = vec![];
     response_monitors_json.iter().try_for_each(|monitor| {
-        let station = WienerLinienLocationStop::assemble_from_json(monitor);
+        let stop_json = monitor["locationStop"].clone();
+
+        let station =
+            serde_json::from_value(stop_json).map_err(ApiRequestError::JsonParsingFailed)?;
+
         let mut v_lines: Vec<WienerLinienLine> = vec![];
+
         if let Some(arr_lines) = monitor["lines"].as_array() {
             arr_lines
                 .iter()
-                .for_each(|line| v_lines.push(WienerLinienLine::assemble_from_json(line)));
+                .for_each(|line| v_lines.push(serde_json::from_value(line.to_owned()).unwrap()));
             wl_monitors.push(WienerLinienMonitor {
                 lines: v_lines,
                 locationStop: station,
@@ -311,37 +256,29 @@ async fn make_api_request(
 
     let mut departures: Vec<Departure> = vec![];
     wl_monitors.iter().for_each(|monitor| {
-        let mut t_lines: Vec<WienerLinienLine> = vec![];
-        monitor.lines.iter().for_each(|line| {
-            t_lines.push(line.to_owned());
-        });
-        t_lines.iter().for_each(|t_line| {
-            t_line.to_owned().departures.iter().for_each(|dep| {
+        let t_lines: Vec<WienerLinienLine> = monitor.lines.to_vec();
+
+        for t_line in t_lines.iter() {
+            for dep in t_line.departures.departure.iter() {
                 departures.push(Departure::from_wiener_linien_api(
-                    t_line.to_owned(),
-                    {
-                        if dep.timeRealIsPresent {
-                            dep.timeReal
-                        } else {
-                            dep.timePlanned
-                        }
-                    },
-                    dep.countdown,
-                    monitor.locationStop.title.to_owned(),
+                    t_line,
+                    &dep.departure_time.time_planned,
+                    &dep.departure_time.time_real,
+                    &dep.departure_time.countdown,
+                    &monitor.locationStop.properties.title,
                 ))
-            })
-        })
+            }
+        }
     });
 
-    let mut traffic_info: Option<Vec<WienerLinienTrafficInfo>> = None;
-    if let Some(response_trafficinfo) = response_trafficinfo_json {
-        traffic_info = Some(
-            response_trafficinfo
+    let traffic_info = response_trafficinfo_json.and_then(|traffic_info_json| {
+        traffic_info_json
             .iter()
-            .map(WienerLinienTrafficInfo::assemble_from_json)
-            .collect() 
-        );
-    }
+            .map(|traffic_info_value| serde_json::from_value(traffic_info_value.to_owned()))
+            .collect::<Result<Vec<WienerLinienTrafficInfo>, _>>()
+            .ok()
+    });
+
     departures.sort();
 
     Ok((departures, traffic_info))
@@ -350,8 +287,6 @@ async fn make_api_request(
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
-        // clear
-        print!("\x1B[2J\x1B[1;1H");
         let (departures, traffic_info) = make_api_request().await.unwrap();
         if traffic_info.is_some() {
             traffic_info.unwrap().iter().for_each(|info| {
@@ -377,5 +312,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 );
             }
         }
+        sleep(Duration::from_secs(10));
     }
 }
